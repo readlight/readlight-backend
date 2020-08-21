0a564e4e0720e6ca4b089d4787a10eaccb3b7643
YunYeongChang <yych4066@naver.com>
2020-08-21 21:39:06 +0900

    feat : user authentification
    + auth/signup.js -> user register, send verification email
    + auth/login.js -> user login, get jwtToken from server
    + auth/jwtdecode.js -> decode jwtToken for user
    + auth/active.js -> email authenfication client - enable
    + models/token.js -> verify token for email-auth
    + models/authlog.js -> save user auth log on database
    + models/user.js -> user model

---
+++ models/authlog.js
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+
+const authlog = new Schema({
+    timestamp: String,
+    causedby: String,
+    originip: String,
+    category: String,
+    details: Object,
+    result: Object,
+    memo: String
+}, {
+    versionKey: false
+});
+
+export default model("authLog", authlog);
