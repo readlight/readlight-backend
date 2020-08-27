e36802227d5d74f6cd4eae1cb2ce755e93044866
YunYeongChang <yych4066@naver.com>
2020-08-28 01:10:38 +0900

    feat : non social user can change account password
    + auth/pwdchange.js -> change non social account password
    * active.js
    * auth/pwdreset/pwdreset.js -> code refactor
    * swagger.yaml -> change password input format to "password"

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -3,6 +3,7 @@ import signup from "./signup";
 import active from "./active";
 import login from "./login";
 import jwtdecode from "./jwtdecode";
+import pwdchange from "./pwdchange";
 import social_kakao from "./social_kakao";
 
 import pwdreset from "./pwdreset/";
@@ -12,6 +13,7 @@ router.use ("/signup", signup);
 router.use ("/active", active);
 router.use ("/login", login);
 router.use ("/jwtdecode",jwtdecode);
+router.use ("/pwdchange", pwdchange);
 router.use ("/pwdreset", pwdreset);
 router.use ("/social-kakao", social_kakao);
 
