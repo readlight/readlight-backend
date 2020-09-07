771b4bbd900254c2f76c18f6b3bf94c89f8d8abd
YunYeongChang <yych4066@naver.com>
2020-09-08 00:09:08 +0900

    refactor : respond in one object instead of string#jwtToken

---
+++ routes/coms/mailconnect.js
@@ -0,0 +1,26 @@
+import { createTransport } from "nodemailer";
+
+var _response = { "transporter" : null, "mailerror" : true };
+const mailConnect = async () => {
+    const transporter = createTransport({
+        host: process.env.MAIL_AUTH_SMTP_HOST,
+        port: process.env.MAIL_AUTH_SMTP_PORT,
+        secure: true,
+        auth: {
+            user: process.env.MAIL_AUTH_USER,
+            pass: process.env.MAIL_AUTH_PASSWORD
+        }
+    });
+    try {
+        const verify = await transporter.verify();
+        if (!verify) throw (verify);
+        _response.transporter = transporter;
+        _response.mailerror = null;
+    } catch (error) {
+        _response.transporter = null;
+        _response.mailerror = error;
+    }
+    return _response;
+};
+
+export default mailConnect;
