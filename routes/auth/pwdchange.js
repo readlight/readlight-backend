56b25cae7ee72b21ff18220aba3ddd52d555d30c
YunYeongChang <yych4066@naver.com>
2020-09-08 00:11:16 +0900

    refactor : export response function outside#pwdreset,pwdchange

--- routes/auth/pwdchange.js
+++ routes/auth/pwdchange.js
@@ -3,69 +3,33 @@ import { pbkdf2Sync } from "crypto";
 import { getClientIp } from "request-ip";
 import { db_error } from "../../app";
 import moment from "moment";
+import loadRegex from "../coms/loadRegex";
+import responseFunction from "../coms/apiResponse";
 import authLog from "../../models/authlog";
 import User from "../../models/user";
 
 const router = Router();
 
-router.put ("/", async (req,res) => {
-    var _response = { "result" : "ERR_SERVER_FAILED_TEMPORARILY" };
-
-    /**
-     * CHECK DATABASE STATE
-     */
-    if (!(db_error === null)) {
-        _response.result = "ERR_DATABASE_NOT_CONNECTED";
-        res.status(500).json(_response);
-        return;
-    }
-
-    /**
-     * CHECK WHETHER PROVIDED POST DATA IS VALID
-     */
-    const { email, curpassword, newpassword } = req.body;
-    const password_chk = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=*()]).*$/;
-    if (!(curpassword && password_chk.test(curpassword) && newpassword && password_chk.test(newpassword))) {
-        _response.result = "ERR_DATA_FORMAT_INVALID";
-        res.status(412).json(_response);
-        return;
-    } else if (curpassword === newpassword) {
-        _response.result = "ERR_USER_PASSWORD_DUPLICATE";
-        res.status(424).json(_response);
-        return;
-    } 
-
-    /**
-     * CHECK AUTHORIZATION HEADER USING BASIC AUTH
-     */
-    if (!(req.headers.authorization === `Basic ${process.env.ACCOUNT_BASIC_AUTH_KEY}`)) {
-        _response.result = "ERR_NOT_AUTHORIZED_IDENTITY";
-        res.status(403).json(_response);
-        return;
-    }
-
-    /**
-     * CHECK WHETHER PROVIDED EMAIL USER EXIST
-     */
+router.put ("/:email", async (req,res) => {
+    //#CHECK DATABASE AND CHECK AUTHORIZATION HEADER USING BASIC AUTH
+    if (!(db_error === null)) return await responseFunction(res, 500, {"msg":"ERR_DATABASE_NOT_CONNECTED"}, null);
+    if (!(req.headers.authorization === `Basic ${process.env.ACCOUNT_BASIC_AUTH_KEY}`)) return await responseFunction(res, 403, {"msg":"ERR_NOT_AUTHORIZED_IDENTITY"}, null);
+    
+    //#CHECK WHETHER PROVIDED POST DATA IS VALID
+    const { curpassword, newpassword } = req.body, { email } = req.params;
+    const { emailchk, passwdchk } = await loadRegex();
+    if (!(email && curpassword && newpassword)) return await responseFunction(res, 412, {"msg":"ERR_DATA_NOT_PROVIDED"}, null);
+    if (!(emailchk.test(email) && passwdchk.test(curpassword) && passwdchk.test(newpassword))) return await responseFunction(res, 412, {"msg":"ERR_DATA_FORMAT_INVALID"}, null);
+    else if (curpassword === newpassword) return await responseFunction(res, 424, {"msg":"ERR_USER_PASSWORD_DUPLICATE"}, null);
+    
+    //#GET USER OBJECT THROUGH EMAIL
     const _user = await User.findOne({"email" : email});
-    if (!_user) {
-        _response.result = "ERR_USER_NOT_FOUND";
-        res.status(409).json(_response);
-        return;
-    } else if (_user.enable === "rejected") {
-        _response.result = "ERR_USER_ACCESS_DENIED";
-        res.status(423).json(_response);
-        return;
-    } else if (_user.enable === "kakao") {
-        _response.result = "ERR_PASSWORD_RESET_NOT_ACCEPTED";
-        res.status(409).json(_response);
-        return;
-    }
+    if (_user === null || _user === undefined) return await responseFunction(res, 409, {"msg":"ERR_USER_NOT_FOUND"}, null);
+    else if (_user.enable === "rejected") return await responseFunction(res, 423, {"msg":"ERR_USER_ACCESS_DENIED"}, null);
+    else if (_user.enable === "kakao") return await responseFunction(res, 409, {"msg":"ERR_PASSWORD_RESET_NOT_ACCEPTED"}, null);
 
-    /**
-     * SAVE LOG FUNCTION
-     */
-    const SAVE_LOG = (_result) => {
+    //#SAVE ACCESS LOG ON DATABASE
+    const SAVE_LOG = async (_response) => {
         const createLog = new authLog ({
             timestamp : moment().format("YYYY-MM-DD HH:mm:ss"), 
             causedby : email,
@@ -76,45 +40,28 @@ router.put ("/", async (req,res) => {
                 "password": req.body.curpassword,
                 "new-password": req.body.newpassword
             },
-            result : _result
+            response : _response
         });
-        
-        createLog.save(async (err) => {
+        await createLog.save(async (err) => {
             if (err) console.error(err);
         });
     };  
 
-    /**
-     * CHECK WHETHER CREDENTIALS USER PROVIDED ACCEPTABLE
-     */
-    const encryptCurPassword = pbkdf2Sync(curpassword, _user.salt, 100000, 64, "SHA512");
-    const encryptNewPassword = pbkdf2Sync(newpassword, _user.salt, 100000, 64, "SHA512");
+    //#CHECK WHETHER CREDENTIALS USER PROVIDED ACCEPTABLE
+    const encryptCurPassword = await pbkdf2Sync(curpassword, _user.salt, 100000, 64, "SHA512");
+    const encryptNewPassword = await pbkdf2Sync(newpassword, _user.salt, 100000, 64, "SHA512");
+    if (!encryptCurPassword) return await responseFunction(res, 500, {"msg":"ERR_PASSWORD_ENCRYPT_FAILED"}, null, encryptCurPassword);
+    if (!encryptNewPassword) return await responseFunction(res, 500, {"msg":"ERR_PASSWORD_ENCRYPT_FAILED"}, null, encryptNewPassword);
     req.body.curpassword = encryptCurPassword.toString("base64"); //HIDE INPUT_PW ON DATABASE
     req.body.newpassword = encryptNewPassword.toString("base64"); //HIDE INPUT_PW ON DATABASE
-    if (encryptCurPassword.toString("base64") !== _user.password) {
-        _response.result = "ERR_USER_AUTH_FAILED";
-        res.status(409).json(_response);
-        SAVE_LOG(_response);
-        return;
-    } 
+    if (encryptCurPassword.toString("base64") !== _user.password) return await SAVE_LOG(await responseFunction(res, 409, {"msg":"ERR_USER_AUTH_FAILED"}, null));
 
-    /**
-     * CHANGE USER EASSWORD TASK
-     */
+    //#RUN USER ACCOUNT PASSWORD CHANGE TASK
     const _verify = await User.updateOne( {"email" : email, "password" : encryptCurPassword.toString("base64")} , {"password" : encryptNewPassword.toString("base64"), "lastlogin" : moment().format("YYYY-MM-DD HH:mm:ss")});
-    if (!_verify) {
-        _response.result = "ERR_USER_PASSWORD_UPDATE_FAILED";
-        res.status(500).json(_response);
-        SAVE_LOG(_response);
-        return;
-    }
+    if (!_verify) return await SAVE_LOG(await responseFunction(res, 500, {"msg":"ERR_USER_PASSWORD_UPDATE_FAILED"}, null, _verify));
 
-    /**
-     * ALL TASK FINISHED, SHOW OUTPUT
-     */
-    _response.result = "SUCCEED_USER_PASSWORD_CHANGED";
-    res.status(200).json(_response);
-    SAVE_LOG(_response);
+    //#ALL TASK FINISHED, SHOW OUTPUT
+    return await SAVE_LOG(await responseFunction(res, 200, {"msg":"SUCCEED_USER_PASSWORD_CHANGED"}, null));
 });
 
 export default router;
